e01e3a359ccaf0a66f96c94a865f82d9ff132184
Gerald Hernandez <gerald2557@gmail.com>
2018-03-07 00:05:37 -0800

    Styled the landing page with final text

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,6 +1,7 @@
 class MailsController < ApplicationController
   def subscribe
     email = params[:email]
+    # gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API'])
     flash[:success] = "Email signed up!"
     redirect_to root_path
   end
